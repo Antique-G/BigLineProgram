ffb4b87e8d1554c25c0b85e4b1e8b0cf8526d817
yannie <yannie@example.com>
2020-11-28 12:30:50 +0800

    修改左边nav

--- src/app/shared/menu-items/menu-items.ts
+++ src/app/shared/menu-items/menu-items.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
+import { MenuService } from 'ng-zorro-antd/menu';
 
 export interface Menu {
+  header: string;
+  menulist: Menus[];
+}
+
+export interface Menus {
   state: string;
   name: string;
   type: string;
   icon: string;
 }
 
-const MENUITEMS = [
-  { state: 'admin', type: 'link', name: '管理员账号模块', icon: 'view_headline' },
-  { state: 'store', type: 'link', name: '店铺管理模块', icon: 'view_list' },
-  { state: 'storeAccount', type: 'link', name: '店铺账号管理模块', icon: 'view_headline' },
-  { state: 'storeBankAccount', type: 'link', name: '店铺银行账号管理模块', icon: 'view_headline' }
 
+const MENUITEMS = [
+  {
+    header: '管理员账号模块',
+    menulist: [{ state: 'admin', type: 'link', name: '管理员账号列表', icon: 'view_headline' }]
+  },
+  {
+    header: '店铺模块',
+    menulist: [{ state: 'store', type: 'link', name: '店铺管理', icon: 'view_list' },
+    { state: 'storeAccount', type: 'link', name: '店铺账号管理', icon: 'view_headline' },
+    { state: 'storeBankAccount', type: 'link', name: '店铺银行账号管理', icon: 'view_headline' }
+    ]
+  }
 ];
 
 
 export interface StoreMenu {
+  storeHeader: string;
+  storeMenulist: StoreMenus[];
+}
+
+export interface StoreMenus {
   state: string;
   name: string;
   type: string;
@@ -24,11 +42,15 @@ export interface StoreMenu {
 }
 
 const StoreMENUITEMS = [
-  { state: 'admin', type: 'link', name: 'Admin', icon: 'view_headline' }
+  {
+    storeHeader: '店铺页面',
+    storeMenulist: [{ state: 'admin', type: 'link', name: 'Admin', icon: 'view_headline' }]
+  }
 ];
 
 @Injectable()
 export class MenuItems {
+
   getMenuitem(): Menu[] {
     return MENUITEMS;
   }
