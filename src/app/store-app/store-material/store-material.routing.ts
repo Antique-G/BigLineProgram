b83f2a1bd53734c3a5f0496ec3bc6cfd691a002f
yannie <yannie@example.com>
2020-12-04 11:37:17 +0800

    产品后台添加产品管理模块的按日期报价

--- src/app/store-app/store-material/store-material.routing.ts
+++ src/app/store-app/store-material/store-material.routing.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { FullComponent } from '../../../app/layouts/full/full.component';
 import { StoreMeetingPlaceComponent } from './store-meeting-place/store-meeting-place.component';
 import { StoreProductManagementComponent } from './store-product-management/store-product-management.component';
+import { StoreQuoteBydateComponent } from './store-quote-bydate/store-quote-bydate.component';
 import { StoreTermsManagementComponent } from './store-terms-management/store-terms-management.component';
 
 export const StoreMaterialRoutes: Routes = [
@@ -14,6 +15,11 @@ export const StoreMaterialRoutes: Routes = [
         path: 'storeProduct',
         component: StoreProductManagementComponent
       },
+      {
+        path: 'storeQuote',
+        component: StoreQuoteBydateComponent
+      },
+      
       {
         path: 'storeMeetingPlace',
         component: StoreMeetingPlaceComponent
