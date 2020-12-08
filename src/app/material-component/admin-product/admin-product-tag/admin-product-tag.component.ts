68d2ad04e09ec1d320f86f487030f64d66e1c36f
yannie <yannie@example.com>
2020-12-08 11:39:44 +0800

    添加产品管理

--- src/app/material-component/admin-product/admin-product-tag/admin-product-tag.component.ts
+++ src/app/material-component/admin-product/admin-product-tag/admin-product-tag.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AdminProductTagService } from '../../../../services/admin/admin-product-tag.service';
-import {AdminProductTagCreateComponent} from './admin-product-tag-create/admin-product-tag-create.component';
+import { AdminProductTagCreateComponent } from './admin-product-tag-create/admin-product-tag-create.component';
 import { AdminProductTagDetailComponent } from './admin-product-tag-detail/admin-product-tag-detail.component';
 
 
@@ -14,9 +14,9 @@ import { AdminProductTagDetailComponent } from './admin-product-tag-detail/admin
 export class AdminProductTagComponent implements OnInit {
   dataSource = [];
   loading = true;
-  
-  constructor(public dialog:MatDialog, public adminProductTagService:AdminProductTagService,) {}
-  
+
+  constructor(public dialog: MatDialog, public adminProductTagService: AdminProductTagService,) { }
+
 
   ngOnInit(): void {
     this.getData();
@@ -25,32 +25,32 @@ export class AdminProductTagComponent implements OnInit {
   getData(): void {
     this.loading = true;
     this.adminProductTagService.getProductTagList().subscribe((result: any) => {
-      console.log("jieguo",result);
+      console.log("jieguo", result);
       this.loading = false;  //总页数
       this.dataSource = result.data;
-      });
-    };
+    });
+  };
 
-  
-  edit(data:any){
-    console.log("传的值",data);
-    const dialogRef = this.dialog.open(AdminProductTagDetailComponent,{
-      width:'500px',
+
+  edit(data: any) {
+    console.log("传的值", data);
+    const dialogRef = this.dialog.open(AdminProductTagDetailComponent, {
+      width: '500px',
       data: data
     })
-    dialogRef.afterClosed().subscribe((result:any)=>{
+    dialogRef.afterClosed().subscribe((result: any) => {
       if (result !== undefined) {
         this.getData();
       }
     })
-    
+
   }
 
-  addTap(){
-    const dialogRef = this.dialog.open(AdminProductTagCreateComponent,{
-      width:'500px'
+  addTap() {
+    const dialogRef = this.dialog.open(AdminProductTagCreateComponent, {
+      width: '500px'
     })
-    dialogRef.afterClosed().subscribe((result:any)=>{
+    dialogRef.afterClosed().subscribe((result: any) => {
       if (result !== undefined) {
         this.getData();
       }
@@ -58,8 +58,17 @@ export class AdminProductTagComponent implements OnInit {
   }
 
 
-  delete(){
-
+  delete(data: any) {
+    console.log("传的值", data);
+    this.adminProductTagService.deleteProductTag(data.id).subscribe(res => {
+      if (res.status_code) {
+        alert("删除失败");
+      }
+      else {
+        alert("删除成功");
+        this.getData();
+      }
+    })
   }
 }
 
