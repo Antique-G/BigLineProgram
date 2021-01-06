8996bc69372d81bfffed21f84134ed085f42e368
Antique-ling <406721821@qq.com>
2021-01-06 18:03:08 +0800

    添加图片

--- src/app/store-app/store-material/store-product-management/store-product-management-detail/store-product-management-detail-desc/store-product-management-detail-desc.component.ts
+++ src/app/store-app/store-material/store-product-management/store-product-management-detail/store-product-management-detail-desc/store-product-management-detail-desc.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store-product-management-detail-desc.component.css']
 })
 export class StoreProductManagementDetailDescComponent implements OnInit {
+  dataSource: any[] = [];   //1.4将数据添加到dataSource
+  page = 1;
+  per_page = 20;
+  total = 1;
+  checked = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  changePageSize(per_page: number) {
+    this.per_page = per_page;
+    // this.getProductList();
+  }
+
+  changePageIndex(page: number) {
+    console.log("当前页", page);
+    this.page = page;
+    // this.getProductList();
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    // this.updateCheckedSet(id, checked);
+    // this.refreshCheckedStatus();
+  }
+
+  onAllChecked(checked: boolean): void {
+    // this.listOfCurrentPageData.filter(({ disabled }) => !disabled).forEach(({ id }) => this.updateCheckedSet(id, checked));
+    // this.refreshCheckedStatus();
+  }
+
+
+  nextTab(){}
 
 }
