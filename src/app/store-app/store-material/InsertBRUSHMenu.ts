94a0dee3ca9f80dc5cba578f0d3dc12d64d5ae12
Antique-ling <406721821@qq.com>
2021-01-12 16:55:10 +0800

    格式刷

--- src/app/store-app/store-material/InsertBRUSHMenu.ts
+++ src/app/store-app/store-material/InsertBRUSHMenu.ts
@@ -6,31 +6,13 @@ export class InsertBRUSHMenu extends BtnMenu {
     constructor(public editor: wangEditor) {
         super(E.$(
             `<div class="w-e-menu">
-                <i class="w-e-icon-image"></i>
+                <i class="iconfont icon-geshishua"></i>
             </div>`), editor)
     }
 
     // modal:any = this.cc
     // 菜单点击事件
     clickHandler() {
-        let config = this.editor.config
-        let collback = config.customFunction
-        if (collback && typeof collback === 'function') {
-            collback(this.insertBrush.bind(this));
-        }
-
-
-    }
-    // 菜单激活状态
-    tryChangeActive() {
-        this.active() // 菜单激活
-        // this.unActive() // 菜单不激活
-    }
-
-
-
-
-    public insertBrush(e: string, type: any): void {
         const editor = this.editor;
         const isSeleEmpty = editor.selection.isSelectionEmpty();
         if (!isSeleEmpty) {
@@ -52,26 +34,36 @@ export class InsertBRUSHMenu extends BtnMenu {
         editor._brush = true
         // 如果是双击格式刷触发连续使用格式刷
         // 记录双击格式刷状态
-        editor._dblBrush = type === 'dblclick' ? true : false
+        // editor._dblBrush = type === 'dblclick' ? true : false
         this.$elem.addClass('w-e-active')
         editor.$textContainerElem.addClass('brush')
         let containerEle = editor.selection.getSelectionContainerElem();
-        let style = containerEle.css('1');
+        let style = containerEle.css();
 
-        while (!containerEle.equal(editor.$textElem[0])) {
-            containerEle = containerEle.parent()
-            if (containerEle.parent().equal(editor.$textElem[0]) && !containerEle.equal(editor.$textElem[0])) {
-                style = Object.assign({}, style, { wrap: containerEle.css('1') })
-            }
-            if (!containerEle.parent().equal(editor.$textElem[0]) && !containerEle.equal(editor.$textElem[0])) {
-                style = Object.assign({}, style, containerEle.css('1'))
-            }
-        }
+        // while (!containerEle.equal(editor.$textElem[0])) {
+        //     containerEle = containerEle.parent()
+        //     if (containerEle.parent().equal(editor.$textElem[0]) && !containerEle.equal(editor.$textElem[0])) {
+        //         style = Object.assign({}, style, { wrap: containerEle.css() })
+        //     }
+        //     if (!containerEle.parent().equal(editor.$textElem[0]) && !containerEle.equal(editor.$textElem[0])) {
+        //         style = Object.assign({}, style, containerEle.css())
+        //     }
+        // }
 
         //保存style
         editor._style = style;
 
+
+
     }
+    // 菜单激活状态
+    tryChangeActive() {
+        this.active() // 菜单激活
+        // this.unActive() // 菜单不激活
+    }
+
+
+
 
 
 
