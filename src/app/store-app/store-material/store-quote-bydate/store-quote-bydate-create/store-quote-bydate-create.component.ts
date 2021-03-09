3bdeb62864bbe49a413d1fa96686956936c4cb19
Antique-ling <406721821@qq.com>
2021-03-09 10:44:15 +0800

    修改产品报价保留两位小数

--- src/app/store-app/store-material/store-quote-bydate/store-quote-bydate-create/store-quote-bydate-create.component.ts
+++ src/app/store-app/store-material/store-quote-bydate/store-quote-bydate-create/store-quote-bydate-create.component.ts
@@ -63,6 +63,7 @@ export class StoreQuoteBydateCreateComponent implements OnInit {
     { label: '周日', value: 0, checked: true },
   ]
 
+  precision=2
 
   validationMessage: any = {
     adult_price: {
@@ -118,11 +119,7 @@ export class StoreQuoteBydateCreateComponent implements OnInit {
       date: ''
     }
     this.buildForm();
-    this.addForm.valueChanges.subscribe(data => {
-      this.onValueChanged(data);
-    });
-    // 初始化错误信息
-    this.onValueChanged();
+   
 
 
   }
@@ -221,30 +218,7 @@ export class StoreQuoteBydateCreateComponent implements OnInit {
     this.isAllowOver = (this.freeTravelModel.allow_over).toString() || 0
   }
 
-  // 表单验证
-  onValueChanged(data?: any) {
-    // 如果表单不存在则返回
-    if (!this.addForm) return;
-    // 获取当前的表单
-    const form = this.addForm;
-    // 遍历错误消息对象
-    for (const field in this.formErrors) {
-      // 清空当前的错误消息
-      this.formErrors[field] = '';
-      // 获取当前表单的控件
-      const control: any = form.get(field);
-      // 当前表单存在此空间控件 && 此控件没有被修改 && 此控件验证不通过
-      if (control && !control.valid) {
-        // 获取验证不通过的控件名，为了获取更详细的不通过信息
-        const messages = this.validationMessage[field];
-        // 遍历当前控件的错误对象，获取到验证不通过的属性
-        for (const key in control.errors) {
-          // 把所有验证不通过项的说明文字拼接成错误消息
-          this.formErrors[field] = messages[key];
-        }
-      }
-    }
-  }
+
 
   getAllDateCN(startTime: Date, endTime: Date) {
     if (!startTime) return
@@ -425,7 +399,17 @@ export class StoreQuoteBydateCreateComponent implements OnInit {
   }
 
 
-  numTest(data: any) {
+  numTest1(data: any) {
+    console.log('data :>> ', data,)
+    data.target.value = data.target.value.replace(/[^\d.]/g, '').replace(/\.{2,}/g, '.').replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');
+  }
+
+  numTest2(data: any) {
+    console.log('data :>> ', data,)
+    data.target.value = data.target.value.replace(/[^\d.]/g, '').replace(/\.{2,}/g, '.').replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');
+  }
+
+  numTest3(data: any) {
     console.log('data :>> ', data,)
     data.target.value = data.target.value.replace(/[^\d.]/g, '').replace(/\.{2,}/g, '.').replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');
   }
