c1ca247ef1f2977bf938c76cd52200c2b4a6b0ad
Antique-ling <406721821@qq.com>
2021-03-19 11:06:31 +0800

    添加退款

--- src/app/material-component/admin-order-refund/admin-order-refund-detail/admin-order-refund-detail.component.ts
+++ src/app/material-component/admin-order-refund/admin-order-refund-detail/admin-order-refund-detail.component.ts
@@ -50,4 +50,6 @@ export class AdminOrderRefundDetailComponent implements OnInit {
     this.selectedTabIndex = 2;
   }
 
+
+  add(){}
 }
--- src/app/shared/menu-items/menu-items.ts
+++ src/app/shared/menu-items/menu-items.ts
@@ -37,9 +37,9 @@ const MENUITEMS = [
       { state: 'orderList', name: '团订单列表' },
       { state: 'groupTravelOrder', name: '跟团游订单列表' },
       { state: 'freeTravelOrder', name: '自由行订单列表' },
-      // { state: 'refund', name: '订单退款处理' },
-      // { state: 'refundReview', name: '订单审核退款' },
-      // { state: 'refundTurnOver', name: '订单退款流水' },
+      { state: 'refund', name: '订单退款处理' },
+      { state: 'refundReview', name: '订单审核退款' },
+      { state: 'refundTurnOver', name: '订单退款流水' },
 
     ]
   },
