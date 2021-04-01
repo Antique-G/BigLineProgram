abe6fd381549b00c9659e64635d05398916b3d79
Antique-ling <406721821@qq.com>
2021-04-01 11:28:34 +0800

    添加模块

--- src/app/store-app/store-login/store-login.module.ts
+++ src/app/store-app/store-login/store-login.module.ts
@@ -9,18 +9,31 @@ import { MaterialComponentsModule } from '../../../app/material-component/materi
 import { StoreForgetPasswordComponent } from './store-forget-password/store-forget-password.component';
 import { StoreRegisteredComponent } from './store-registered/store-registered.component';
 import { StoreSetNewPasswordComponent } from './store-forget-password/store-set-new-password/store-set-new-password.component';
+import { StoreBannerComponent } from './store-banner/store-banner.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { DemoNgZorroAntdModule } from '../../../app/ng-zorro-antd.module';
 
 
 @NgModule({
-  declarations: [StoreLoginComponent, StoreForgetPasswordComponent, StoreRegisteredComponent, StoreSetNewPasswordComponent],
+  declarations: [StoreLoginComponent, StoreForgetPasswordComponent, StoreRegisteredComponent, StoreSetNewPasswordComponent, StoreBannerComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     StoreLoginRoutingModule,
     DemoMaterialModule,
+    FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialComponentsModule
+    MaterialComponentsModule,
+    CdkTableModule,
+    DemoNgZorroAntdModule,
+    NzTimePickerModule,
+    ScrollingModule,
+    DragDropModule
   ]
 })
 export class StoreLoginModule { }
