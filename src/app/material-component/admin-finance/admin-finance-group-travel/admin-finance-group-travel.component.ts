0c58fba1418a4c57ff4ac052850a51bbbb20e3cc
Antique-ling <406721821@qq.com>
2021-05-11 17:33:29 +0800

    添加展示财务模块信息,导出excel

--- src/app/material-component/admin-finance/admin-finance-group-travel/admin-finance-group-travel.component.ts
+++ src/app/material-component/admin-finance/admin-finance-group-travel/admin-finance-group-travel.component.ts
@@ -270,4 +270,24 @@ export class AdminFinanceGroupTravelComponent implements OnInit {
             this.groupTravel();
         })
     }
+
+
+
+    // 导出
+    export() {
+        this.setValue();
+        this.status = this.status == null ? '' : this.status;
+        this.date_start = this.date_start == null ? '' : this.date_start;
+        this.date_end = this.date_end == null ? '' : this.date_end;
+        this.order_start_date = this.order_start_date == null ? '' : this.order_start_date;
+        this.order_end_date = this.order_end_date == null ? '' : this.order_end_date;
+        this.payment_status= this.payment_status == null ? '' : this.payment_status;
+        this.isExport = this.api + '/admin/order/export?page=' + this.page + '&per_page=' + this.per_page + '&status=' + this.status +
+            '&product_id=' + this.product_id + '&product_name=' + this.product_name + '&order_number=' + this.order_number +
+            '&date_start=' + this.date_start + '&date_end=' + this.date_end + '&product_code=' + this.product_code +
+            '&store_id=' + this.store_id + '&order_start_date=' + this.order_start_date + '&order_end_date=' + this.order_end_date +
+            '&contact_name=' + this.contact_name + '&contact_phone=' + this.contact_phone + '&payment_status=' + this.payment_status;
+
+            console.log('object :>> ', this.isExport);
+    }
 }
