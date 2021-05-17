8813aebcaa488bd69a8a8ff17af32ee74c218ccc
Antique-ling <406721821@qq.com>
2021-05-17 18:23:56 +0800

    保险列表设置基础保险，有且只有一个，产品添加根据设置的基础保险直接赋值

--- src/app/material-component/admin-insurance/admin-insurance-create/admin-insurance-create.component.ts
+++ src/app/material-component/admin-insurance/admin-insurance-create/admin-insurance-create.component.ts
@@ -4,146 +4,150 @@ import { AdminInsuranceCreateRequestModel } from "../../../../interfaces/adminIn
 import { AdminInsuranceService } from "../../../../services/admin/admin-insurance.service";
 
 @Component({
-  selector: "app-admin-insurance-create",
-  templateUrl: "./admin-insurance-create.component.html",
-  styleUrls: ["./admin-insurance-create.component.css"],
+    selector: "app-admin-insurance-create",
+    templateUrl: "./admin-insurance-create.component.html",
+    styleUrls: ["./admin-insurance-create.component.css"],
 })
 export class AdminInsuranceCreateComponent implements OnInit {
-  validateForm!: FormGroup;
-  adminInsuranceCreateRequestModel: AdminInsuranceCreateRequestModel;
-  status = '1';
+    validateForm!: FormGroup;
+    adminInsuranceCreateRequestModel: AdminInsuranceCreateRequestModel;
+    status = '1';
 
 
-  validationMessage: any = {
-    code: {
-      maxlength: "保险产品方案代码长度最多为20个字符",
-      required: "请输入保险产品方案代码！",
-    },
-    name: {
-      maxlength: "保险名称长度最多为32个字符",
-      required: "请输入保险名称！",
-    },
-    article: {
-      required: "请输入保险条款！",
-    }
-  };
-  
-  formErrors: any = {
-    name: '',
-    code: '',
-    article: '',
-  };
-  
-
-
-  constructor(private fb: FormBuilder, private adminInsuranceService: AdminInsuranceService) {
-    this.forms();
-    this.adminInsuranceCreateRequestModel = {
-      name: "",
-      insured_date: [],
-      status: 1,
-      code: "",
-      insurance_amount: "",
-      article: "",
+    validationMessage: any = {
+        code: {
+            maxlength: "保险产品方案代码长度最多为20个字符",
+            required: "请输入保险产品方案代码！",
+        },
+        name: {
+            maxlength: "保险名称长度最多为32个字符",
+            required: "请输入保险名称！",
+        },
+        article: {
+            required: "请输入保险条款！",
+        }
     };
-  }
+
+    formErrors: any = {
+        name: '',
+        code: '',
+        article: '',
+    };
 
 
-  forms() {
-    this.validateForm = this.fb.group({
-      name: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
-      code: new FormControl(null, [Validators.required, Validators.maxLength(20)]),
-      insured_date: new FormArray([new FormControl(null, [Validators.required, Validators.min(0)])]),
-      insurance_amount: new FormControl(0),
-      article: new FormControl(null, [Validators.required]),
-      status: new FormControl('1'),
-    });
-    // 每次表单数据发生变化的时候更新错误信息
-    this.validateForm.valueChanges.subscribe((data) => {
-      this.onValueChanged(data);
-    });
-    // 初始化错误信息
-    this.onValueChanged();
-  }
+
+    constructor(private fb: FormBuilder, private adminInsuranceService: AdminInsuranceService) {
+        this.forms();
+        this.adminInsuranceCreateRequestModel = {
+            name: "",
+            insured_date: [],
+            status: 1,
+            code: "",
+            insurance_amount: "",
+            article: "",
+            is_base: "",
+        };
+    }
 
 
-  // 表单验证
-  onValueChanged(data?: any) {
-    // 如果表单不存在则返回
-    if (!this.validateForm) return;
-    // 获取当前的表单
-    const form = this.validateForm;
-    // 遍历错误消息对象
-    for (const field in this.formErrors) {
-      // 清空当前的错误消息
-      this.formErrors[field] = "";
-      // 获取当前表单的控件
-      const control: any = form.get(field);
-      // 当前表单存在此空间控件 && 此控件没有被修改 && 此控件验证不通过
-      if (control && !control.valid) {
-        // 获取验证不通过的控件名，为了获取更详细的不通过信息
-        const messages = this.validationMessage[field];
-        // 遍历当前控件的错误对象，获取到验证不通过的属性
-        for (const key in control.errors) {
-          // 把所有验证不通过项的说明文字拼接成错误消息
-          this.formErrors[field] = messages[key];
+    forms() {
+        this.validateForm = this.fb.group({
+            name: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
+            code: new FormControl(null, [Validators.required, Validators.maxLength(20)]),
+            insured_date: new FormArray([new FormControl(null, [Validators.required, Validators.min(0)])]),
+            insurance_amount: new FormControl(0),
+            article: new FormControl(null, [Validators.required]),
+            status: new FormControl('1'),
+            is_base: new FormControl('0', [Validators.required]),
+        });
+        // 每次表单数据发生变化的时候更新错误信息
+        this.validateForm.valueChanges.subscribe((data) => {
+            this.onValueChanged(data);
+        });
+        // 初始化错误信息
+        this.onValueChanged();
+    }
+
+
+    // 表单验证
+    onValueChanged(data?: any) {
+        // 如果表单不存在则返回
+        if (!this.validateForm) return;
+        // 获取当前的表单
+        const form = this.validateForm;
+        // 遍历错误消息对象
+        for (const field in this.formErrors) {
+            // 清空当前的错误消息
+            this.formErrors[field] = "";
+            // 获取当前表单的控件
+            const control: any = form.get(field);
+            // 当前表单存在此空间控件 && 此控件没有被修改 && 此控件验证不通过
+            if (control && !control.valid) {
+                // 获取验证不通过的控件名，为了获取更详细的不通过信息
+                const messages = this.validationMessage[field];
+                // 遍历当前控件的错误对象，获取到验证不通过的属性
+                for (const key in control.errors) {
+                    // 把所有验证不通过项的说明文字拼接成错误消息
+                    this.formErrors[field] = messages[key];
+                }
+            }
         }
-      }
     }
-  }
 
 
-  ngOnInit(): void { }
+    ngOnInit(): void { }
 
-  get insuredDateArray() {
-    return this.validateForm.get("insured_date") as FormArray;
-  }
-
-  //添加
-  add() {
-    this.insuredDateArray.push(new FormControl(null, [Validators.required, Validators.min(0),]));
-  }
-
-  //删除
-  remove(index: number) {
-    if (this.insuredDateArray.length > 1) {
-      this.insuredDateArray.removeAt(index);
+    get insuredDateArray() {
+        return this.validateForm.get("insured_date") as FormArray;
     }
-  }
 
-  setValue() {
-    let dataList = this.validateForm.value.insured_date;
-    dataList.forEach((value: any, index: any) => {
-      if (dataList.length > 1) {
-        if (value == null || value == "" || value == "0") {
-          this.insuredDateArray.removeAt(index);
+    //添加
+    add() {
+        this.insuredDateArray.push(new FormControl(null, [Validators.required, Validators.min(0),]));
+    }
+
+    //删除
+    remove(index: number) {
+        if (this.insuredDateArray.length > 1) {
+            this.insuredDateArray.removeAt(index);
         }
-      }
-    });
-    this.adminInsuranceCreateRequestModel.name = this.validateForm.value.name;
-    this.adminInsuranceCreateRequestModel.code = this.validateForm.value.code;
-    this.adminInsuranceCreateRequestModel.insurance_amount = this.validateForm.value.insurance_amount;
-    this.adminInsuranceCreateRequestModel.article = this.validateForm.value.article;
-    this.adminInsuranceCreateRequestModel.status = this.validateForm.value.status;
-    this.adminInsuranceCreateRequestModel.insured_date = this.validateForm.value.insured_date;
-  }
-
-
-
-  submitForm(): void {
-    this.setValue();
-    for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
     }
-    console.log("提交结果", this.adminInsuranceCreateRequestModel);
-    if (this.validateForm.valid) {
-      this.adminInsuranceService.addAdminInsurance(this.adminInsuranceCreateRequestModel).subscribe((res) => {
-        console.log("jieguo", res);
-        if (res === null) { }
-        else { }
-      }
-      );
+
+    setValue() {
+        let dataList = this.validateForm.value.insured_date;
+        dataList.forEach((value: any, index: any) => {
+            if (dataList.length > 1) {
+                if (value == null || value == "" || value == "0") {
+                    this.insuredDateArray.removeAt(index);
+                }
+            }
+        });
+        this.adminInsuranceCreateRequestModel.name = this.validateForm.value.name;
+        this.adminInsuranceCreateRequestModel.code = this.validateForm.value.code;
+        this.adminInsuranceCreateRequestModel.insurance_amount = this.validateForm.value.insurance_amount;
+        this.adminInsuranceCreateRequestModel.article = this.validateForm.value.article;
+        this.adminInsuranceCreateRequestModel.status = this.validateForm.value.status;
+        this.adminInsuranceCreateRequestModel.insured_date = this.validateForm.value.insured_date;
+        this.adminInsuranceCreateRequestModel.is_base = this.validateForm.value.is_base;
+
     }
-  }
+
+
+
+    submitForm(): void {
+        this.setValue();
+        for (const i in this.validateForm.controls) {
+            this.validateForm.controls[i].markAsDirty();
+            this.validateForm.controls[i].updateValueAndValidity();
+        }
+        console.log("提交结果", this.adminInsuranceCreateRequestModel);
+        if (this.validateForm.valid) {
+            this.adminInsuranceService.addAdminInsurance(this.adminInsuranceCreateRequestModel).subscribe((res) => {
+                console.log("jieguo", res);
+                if (res === null) { }
+                else { }
+            }
+            );
+        }
+    }
 }
--- src/app/material-component/admin-insurance/admin-insurance-detail/admin-insurance-detail.component.ts
+++ src/app/material-component/admin-insurance/admin-insurance-detail/admin-insurance-detail.component.ts
@@ -6,132 +6,135 @@ import { Observable, Observer } from 'rxjs';
 // import { isNumber } from '../../../../app/util/validators';
 
 @Component({
-  selector: 'app-admin-insurance-detail',
-  templateUrl: './admin-insurance-detail.component.html',
-  styleUrls: ['./admin-insurance-detail.component.css']
+    selector: 'app-admin-insurance-detail',
+    templateUrl: './admin-insurance-detail.component.html',
+    styleUrls: ['./admin-insurance-detail.component.css']
 })
 export class AdminInsuranceDetailComponent implements OnInit {
-  validateForm!: FormGroup;
-  insuranceDetailModel!:InsuranceDetailModel;
-  adminInsuranceUpdateRequestModel!: AdminInsuranceUpdateRequestModel;
-  status = '1';
+    validateForm!: FormGroup;
+    insuranceDetailModel!: InsuranceDetailModel;
+    adminInsuranceUpdateRequestModel!: AdminInsuranceUpdateRequestModel;
+    status = '1';
 
 
-  @Input() data:any;
+    @Input() data: any;
 
-  validationMessage: any = {
-    code: {
-      maxlength: "保险产品方案代码长度最多为20个字符",
-      required: "请输入保险产品方案代码！",
-    },
-    name: {
-      maxlength: "保险名称长度最多为32个字符",
-      required: "请输入保险名称！",
-    },
-    article: {
-      required: "请输入保险条款！",
-    }
-  };
-  formErrors: any = {
-    name: '',
-    code: '',
-    article: '',
-  };
-  
-  constructor(public fb:FormBuilder,private adminInsuranceService:AdminInsuranceService) { 
-    this.forms();
-    this.adminInsuranceUpdateRequestModel = {
-      name : '',
-      insured_date: [],
-      id: 0,
-      status: 1,
-      code: "",
-      insurance_amount: "",
-      article: "",
-    }
-  };
-  forms(){
-    this.validateForm = this.fb.group({
-      name: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
-      code: new FormControl(null, [Validators.required, Validators.maxLength(20)]),
-      insured_date: new FormArray([new FormControl(null, [Validators.required, Validators.min(0)])]),
-      insurance_amount: new FormControl(0),
-      article: new FormControl(null, [Validators.required]),
-      status: new FormControl('1'),
-    });
-  
-  };
-
-  ngOnInit(): void {
-   console.log('detail',this.data)
-    this.insuranceDetailModel = this.data;
-    this.patchValue();
-  }
-  get insuredDateArray() {
-    return this.validateForm.get('insured_date') as FormArray;
-  }
-  //添加
-  add() {
-    this.insuredDateArray.push(new FormControl(null,[Validators.required,Validators.min(0),]));
-  }
-  //删除
-  remove(index: number) {
-    if(this.insuredDateArray.length > 1) {
-      this.insuredDateArray.removeAt(index);
-    }
-  }
-  removeNull(){
-    let dataList = this.insuredDateArray.value;
-    dataList.forEach((value:any,index:any) =>{
-      if (value == null || value == 0 || value == '') {
-        this.insuredDateArray.removeAt(index);
-      }
-    })
-  }
-  //赋值
-  patchValue() {
-    this.removeNull();
-    const setValue = this.insuranceDetailModel.insured_date;
-    setValue.forEach((value:any) => {
-      this.insuredDateArray.push(new FormControl(value));
-    });
-    console.log('insuredDateArray',this.insuredDateArray.value)
-  }
-
-
-
-
-  setValue(){
-    if(this.insuredDateArray.length > 1) {
-      this.removeNull();
-    }
-    this.adminInsuranceUpdateRequestModel.name = this.validateForm.value.name;
-    this.adminInsuranceUpdateRequestModel.code = this.validateForm.value.code;
-    this.adminInsuranceUpdateRequestModel.insurance_amount = this.validateForm.value.insurance_amount;
-    this.adminInsuranceUpdateRequestModel.article = this.validateForm.value.article;
-    this.adminInsuranceUpdateRequestModel.status = this.validateForm.value.status;
-    this.adminInsuranceUpdateRequestModel.insured_date = this.insuredDateArray.value;
-    this.adminInsuranceUpdateRequestModel.id = this.insuranceDetailModel.id;
-    console.log('this.adminInsuranceUpdateRequestModel',this.adminInsuranceUpdateRequestModel)
-  }
-
-
-
-  update(){
-    this.setValue();
-    for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
-    }
-    if ( this.validateForm.valid ) {
-      this.adminInsuranceService.insuranceUpdate(this.adminInsuranceUpdateRequestModel).subscribe(res => {
-        console.log('res结果',res)
-        if (res === null) {
-
-        } else {
-          
+    validationMessage: any = {
+        code: {
+            maxlength: "保险产品方案代码长度最多为20个字符",
+            required: "请输入保险产品方案代码！",
+        },
+        name: {
+            maxlength: "保险名称长度最多为32个字符",
+            required: "请输入保险名称！",
+        },
+        article: {
+            required: "请输入保险条款！",
         }
-      })
+    };
+    formErrors: any = {
+        name: '',
+        code: '',
+        article: '',
+    };
+
+    constructor(public fb: FormBuilder, private adminInsuranceService: AdminInsuranceService) {
+        this.forms();
+        this.adminInsuranceUpdateRequestModel = {
+            name: '',
+            insured_date: [],
+            id: 0,
+            status: 1,
+            code: "",
+            insurance_amount: "",
+            article: "",
+            is_base: "",
+        }
+    };
+    forms() {
+        this.validateForm = this.fb.group({
+            name: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
+            code: new FormControl(null, [Validators.required, Validators.maxLength(20)]),
+            insured_date: new FormArray([new FormControl(null, [Validators.required, Validators.min(0)])]),
+            insurance_amount: new FormControl(0),
+            article: new FormControl(null, [Validators.required]),
+            status: new FormControl('1'),
+            is_base: new FormControl('0', [Validators.required]),
+        });
+
+    };
+
+    ngOnInit(): void {
+        console.log('detail', this.data)
+        this.insuranceDetailModel = this.data;
+        this.patchValue();
     }
-  }
+    get insuredDateArray() {
+        return this.validateForm.get('insured_date') as FormArray;
+    }
+    //添加
+    add() {
+        this.insuredDateArray.push(new FormControl(null, [Validators.required, Validators.min(0),]));
+    }
+    //删除
+    remove(index: number) {
+        if (this.insuredDateArray.length > 1) {
+            this.insuredDateArray.removeAt(index);
+        }
+    }
+    removeNull() {
+        let dataList = this.insuredDateArray.value;
+        dataList.forEach((value: any, index: any) => {
+            if (value == null || value == 0 || value == '') {
+                this.insuredDateArray.removeAt(index);
+            }
+        })
+    }
+    //赋值
+    patchValue() {
+        this.removeNull();
+        const setValue = this.insuranceDetailModel.insured_date;
+        setValue.forEach((value: any) => {
+            this.insuredDateArray.push(new FormControl(value));
+        });
+        console.log('insuredDateArray', this.insuredDateArray.value)
+    }
+
+
+
+
+    setValue() {
+        if (this.insuredDateArray.length > 1) {
+            this.removeNull();
+        }
+        this.adminInsuranceUpdateRequestModel.name = this.validateForm.value.name;
+        this.adminInsuranceUpdateRequestModel.code = this.validateForm.value.code;
+        this.adminInsuranceUpdateRequestModel.insurance_amount = this.validateForm.value.insurance_amount;
+        this.adminInsuranceUpdateRequestModel.article = this.validateForm.value.article;
+        this.adminInsuranceUpdateRequestModel.status = this.validateForm.value.status;
+        this.adminInsuranceUpdateRequestModel.insured_date = this.insuredDateArray.value;
+        this.adminInsuranceUpdateRequestModel.id = this.insuranceDetailModel.id;
+        this.adminInsuranceUpdateRequestModel.is_base = this.validateForm.value.is_base;
+        console.log('this.adminInsuranceUpdateRequestModel', this.adminInsuranceUpdateRequestModel)
+    }
+
+
+
+    update() {
+        this.setValue();
+        for (const i in this.validateForm.controls) {
+            this.validateForm.controls[i].markAsDirty();
+            this.validateForm.controls[i].updateValueAndValidity();
+        }
+        if (this.validateForm.valid) {
+            this.adminInsuranceService.insuranceUpdate(this.adminInsuranceUpdateRequestModel).subscribe(res => {
+                console.log('res结果', res)
+                if (res === null) {
+
+                } else {
+
+                }
+            })
+        }
+    }
 }
