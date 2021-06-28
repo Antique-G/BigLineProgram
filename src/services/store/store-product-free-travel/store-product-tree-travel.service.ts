8d9c9adac686882b517230253f7c2baaaa311d42
Antique-ling <406721821@qq.com>
2021-06-28 15:02:31 +0800

    修改预售筛选条件

--- src/services/store/store-product-free-travel/store-product-tree-travel.service.ts
+++ src/services/store/store-product-free-travel/store-product-tree-travel.service.ts
@@ -126,17 +126,14 @@ export class StoreProductTreeTravelService {
 
 
     // 预售产品列表
-    GetPreFreeTravelList(page: number, per_page: number, status: any, check_status: any, title: any,
-        few_days: any, id: any, tag?: any, departure_city?: any, destination_city?: any,
+    GetPreFreeTravelList(page: number, per_page: number,title: any,
+        few_days: any, id: any, departure_city?: any, destination_city?: any,
         start_date?: any, end_date?: any, use_start_date?: any, use_end_date?: any): Observable<any> {
         const params = new HttpParams({ encoder: new EncodeComponent() }).set('page', page.toString())
             .set('per_page', per_page.toString())
-            .set('check_status', check_status ? check_status : '')
             .set('title', title ? title : '')
             .set('few_days', few_days ? few_days : '')
             .set('id', id ? id : '')
-            .set('status', status ? status : '')
-            .set('tag', tag ? tag : '')
             .set('departure_city', departure_city ? departure_city : '')
             .set('destination_city', destination_city ? destination_city : '')
             .set('start_date', start_date ? start_date : '')
